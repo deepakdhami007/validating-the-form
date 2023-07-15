ced6a00106e50dd94e76bbd0b724d7700f745925
deepakdhami007 <deepaksinghdhami01@gmail.com>
2023-07-15 07:07:43 +0530

    adminpage

--- src/App.js
+++ src/App.js
@@ -1,109 +1,104 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
-const OrderForm = () => {
-  const [tableNumber, setTableNumber] = useState('');
-  const [orderId, setOrderId] = useState('');
-  const [price, setPrice] = useState('');
-  const [meal, setMeal] = useState('');
-  const [submittedOrder, setSubmittedOrder] = useState(null);
+const AdminPage = () => {
+  const [productId, setProductId] = useState('');
+  const [sellingPrice, setSellingPrice] = useState('');
+  const [productName, setProductName] = useState('');
+  const [products, setProducts] = useState([]);
+  const [totalStockValue, setTotalStockValue] = useState(0);
 
-  const handleTableNumberChange = (event) => {
-    setTableNumber(event.target.value);
+  // Load products from local storage on component mount
+  useEffect(() => {
+    const storedProducts = localStorage.getItem('products');
+    if (storedProducts) {
+      setProducts(JSON.parse(storedProducts));
+    }
+  }, []);
+
+  // Update total stock value whenever products change
+  useEffect(() => {
+    const stockValue = products.reduce((total, product) => {
+      return total + product.sellingPrice;
+    }, 0);
+    setTotalStockValue(stockValue);
+  }, [products]);
+
+  const handleProductIdChange = (event) => {
+    setProductId(event.target.value);
   };
-  
 
-  const handleOrderIdChange = (event) => {
-    setOrderId(event.target.value);
+  const handleSellingPriceChange = (event) => {
+    setSellingPrice(event.target.value);
   };
 
-  const handlePriceChange = (event) => {
-    setPrice(event.target.value);
+  const handleProductNameChange = (event) => {
+    setProductName(event.target.value);
   };
 
-  const handleMealChange = (event) => {
-    setMeal(event.target.value);
-  };
-
-  const handleFormSubmit = (event) => {
+  const handleAddProduct = (event) => {
     event.preventDefault();
 
-    // Create an order object using the collected data
-    const order = {
-      tableNumber: tableNumber,
-      orderId: orderId,
-      price: price,
-      meal: meal
-    };
+    if (productId && sellingPrice && productName) {
+      const newProduct = {
+        id: productId,
+        sellingPrice: Number(sellingPrice),
+        name: productName
+      };
 
-    // Set the submitted order in state
-    setSubmittedOrder(order);
+      const updatedProducts = [...products, newProduct];
+      setProducts(updatedProducts);
 
-    // Reset the form
-    setTableNumber('');
-    setOrderId('');
-    setPrice('');
-    setMeal('');
+      // Save products to local storage
+      localStorage.setItem('products', JSON.stringify(updatedProducts));
+
+      // Reset form fields
+      setProductId('');
+      setSellingPrice('');
+      setProductName('');
+    }
   };
 
   return (
     <div>
-      <h1>Restaurant Order Form</h1>
-      <form onSubmit={handleFormSubmit}>
-        <div>
-          <label htmlFor="tableNumber">Table Number:</label>
-          <select
-            id="tableNumber"
-            value={tableNumber}
-            onChange={handleTableNumberChange}
-          >
-            <option value="">Select table number</option>
-            <option value="1">Table 1</option>
-            <option value="2">Table 2</option>
-            <option value="3">Table 3</option>
-            <option value="4">Table 4</option>
-          </select>
-        </div>
-        <div>
-          <label htmlFor="orderId">Order ID:</label>
-          <input
-            type="text"
-            id="orderId"
-            value={orderId}
-            onChange={handleOrderIdChange}
-          />
-        </div>
-        <div>
-          <label htmlFor="price">Price:</label>
-          <input
-            type="text"
-            id="price"
-            value={price}
-            onChange={handlePriceChange}
-          />
-        </div>
-        <div>
-          <label htmlFor="meal">Meal:</label>
-          <input
-            type="text"
-            id="meal"
-            value={meal}
-            onChange={handleMealChange}
-          />
-        </div>
-        <button type="submit">Submit Order</button>
-      </form>
-
-      {submittedOrder && (
-        <div>
-          <h2>Submitted Order</h2>
-          <p>Table Number: {submittedOrder.tableNumber}</p>
-          <p>Order ID: {submittedOrder.orderId}</p>
-          <p>Price: {submittedOrder.price}</p>
-          <p>Meal: {submittedOrder.meal}</p>
-        </div>
-      )}
+      <h1>Seller's Admin Page</h1>
+      <div>
+        <h2>Add Product</h2>
+        <form onSubmit={handleAddProduct}>
+          <div>
+            <label htmlFor="productId">Product ID:</label>
+            <input
+              type="text"
+              id="productId"
+              value={productId}
+              onChange={handleProductIdChange}
+            />
+          </div>
+          <div>
+            <label htmlFor="sellingPrice">Selling Price:</label>
+            <input
+              type="text"
+              id="sellingPrice"
+              value={sellingPrice}
+              onChange={handleSellingPriceChange}
+            />
+          </div>
+          <div>
+            <label htmlFor="productName">Product Name:</label>
+            <input
+              type="text"
+              id="productName"
+              value={productName}
+              onChange={handleProductNameChange}
+            />
+          </div>
+          <button type="submit">Add Product</button>
+        </form>
+      </div>
+      <div>
+        <h2>Total Stock Value: {totalStockValue}</h2>
+      </div>
     </div>
   );
 };
 
-export default OrderForm;
+export default AdminPage;
